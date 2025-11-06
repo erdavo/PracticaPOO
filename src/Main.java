ba23866ebcc2acafa9f2edc6dab25b9f6b9081a0
David <doto0992@gmail.com>
2025-11-06 11:19:18 +0100

    He añadido un TODO de prueba

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 void main() {
     IO.println(String.format("Hello and welcome!"));
     for (int i = 1; i <= 5; i++) {
-
-        IO.println("i = " + i);
+        // TODO de prueba
+        System.out.println("Hola que tal");
     }
 }
