c23acfb63110fa9f49351dc487507bbc73749c5f
David <doto0992@gmail.com>
2025-11-06 11:28:41 +0100

    prueba mala

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ void main() {
     for (int i = 1; i <= 5; i++) {
         // TODO de prueba
         System.out.println("Hola que tal");
+
+        System.out.println("hasioahsdfkjahsdflkasjfhasdf");
     }
 }
